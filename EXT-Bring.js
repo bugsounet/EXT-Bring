122918b26bada558411f100388ccf435502592b9
bugsounet <github@bugsounet.fr>
2022-03-17 02:47:43 +0100

    review begin

--- EXT-Bring.js
+++ EXT-Bring.js
@@ -1,4 +1,5 @@
 /* global Module */
+
 Module.register("EXT-Bring", {
   defaults: {
     debug: false,
@@ -34,11 +35,15 @@ Module.register("EXT-Bring", {
         tr-TR
     **/
   },
+
   listData: null,
+
   requiresVersion: "2.18.0",
+
   start: function () {
     this.sendSocketNotification("bringList-REGISTER", this.config)
   },
+
   socketNotificationReceived: function (notification, payload) {
     if (notification === "bringList-LISTUPDATE") {
       // set dataNotification
@@ -47,28 +52,25 @@ Module.register("EXT-Bring", {
         this.updateDom(100)
       }
     }
-    if (notification == "ALL_MODULES_STARTED") {
-      this.updateDom()
-    }
   },
+
   getDom: function () {
     var htmlTemplate = "\n\t\t<div class=\"bringitemcontainer\">\n\t\t\t<div class=\"bringitem\">\n\t\t\t\t<div class=\"itemupper\">\n\t\t\t\t\t<div class=\"indicators-container\"></div>\n\t\t\t\t\t<div class=\"image-container\">\n\t\t\t\t\t\t<img src=\"{iconUrl}\" class=\"itemimage\"/>\n\t\t\t\t\t</div>\n\t\t\t\t\t<div class=\"indicators-container\"></div>\n\t\t\t\t</div>\n\t\t\t\t<div class=\"itemlower\">\n\t\t\t\t\t<div class=\"itemtext-name bright\">{itemName}</div>\n\t\t\t\t\t<div class=\"itemtext-spec normal\">{itemSpec}</div>\n\t\t\t\t</div>\n\t\t\t</div>\n\t\t</div>"
-    var self = this
     // create element wrapper for show into the module
     var wrapper = document.createElement("div")
     // If this.dataRequest is not empty
-    var width = (self.config.columns * 101) + 1
-    var height = (self.config.maxrows * 119)
-    var lastrowstartpercent = Math.round(((self.config.maxrows - 1) / self.config.maxrows) * 100)
+    var width = (this.config.columns * 101) + 1
+    var height = (this.config.maxrows * 119)
+    var lastrowstartpercent = Math.round(((this.config.maxrows - 1) / this.config.maxrows) * 100)
     var contentHtml = '<div style="max-width:' + width + 'px;max-height:' + height + 'px;position:relative;overflow-y:hidden;">'
-    if (self.listData) {
-      var listContent = self.listData
+    if (this.listData) {
+      var listContent = this.listData
       listContent.items.forEach(function (element) {
         var itemHtml = htmlTemplate
           .replace(/\{iconUrl\}/g, element.imagePath)
           .replace(/\{itemName\}/g, element.localName)
-          .replace(/\{itemSpec\}/g, element.specification);
-        contentHtml += itemHtml;
+          .replace(/\{itemSpec\}/g, element.specification)
+        contentHtml += itemHtml
       })
       wrapper.innerHTML = contentHtml
     }
@@ -77,7 +79,7 @@ Module.register("EXT-Bring", {
   
   getStyles: function () {
     return [
-      "mmm-bring-list.css",
+      "EXT-Bring.css"
     ]
   },
 
@@ -88,4 +90,5 @@ Module.register("EXT-Bring", {
   resume: function () {
     this.sendSocketNotification("bringList-REGISTER", this.config)
   }
+
 });
--- node_helper.js
+++ node_helper.js
@@ -1,61 +1,70 @@
 "use strict";
-/* Magic Mirror
- * Node Helper: bringList
+/** Magic Mirror
+ * Node Helper: EXT-Bring
  *
  * By Robert Seidt
+ * modified by @bugsounet
  * MIT Licensed.
- */
+ **/
+
+/** todo: review code in deep
+ * apply @bugsounet style
+ * add EXT Compatibility
+ **/
+
 var NodeHelper = require("node_helper");
 var bring = require("./lib/bring-lib");
+var logBring = (...args) => { /* do nothing */ }
 
 module.exports = NodeHelper.create({
   updater: new bring.BringUpdater(),
   running: false,
   currentInterval: null,
   intervalTime: 60000,
-  logger: null,
+
   socketNotificationReceived: function (notification, payload) {
     if (notification === "bringList-REGISTER") {
-      this.logger = new bring.BringLogger(payload);
-      this.logger.log("Received registration notification.", true);
-      this.updater.register(payload, this.logger);
-      this.intervalTime = Math.max(30000, payload.updateInterval);
-      this.stopLoop();
-      this.ensureLoop();
+      if (payload.debug) logBring = (...args) => { console.log("[BRING]", ...args) }
+      logBring("Received registration notification.")
+      this.updater.register(payload)
+      this.intervalTime = Math.max(30000, payload.updateInterval)
+      this.stopLoop()
+      this.ensureLoop()
     }
     if (notification === "bringList-SUSPEND") {
-      this.logger.log("Received suspend notification.", true);
-      this.updater.unregister(payload);
+      logBring("Received suspend notification.")
+      this.updater.unregister(payload)
       if (!this.updater.hasJobs()) {
-        this.stopLoop();
+        this.stopLoop()
       }
     }
   },
+
   stopLoop: function () {
     if (this.running) {
-      this.logger.log("Loop running. Stopping.", true);
-      clearInterval(this.currentInterval);
-      this.running = false;
-      this.currentInterval = null;
+      logBring("Loop running. Stopping.")
+      clearInterval(this.currentInterval)
+      this.running = false
+      this.currentInterval = null
     }
   },
+
   ensureLoop: function () {
-    var _this = this;
     if (!this.running) {
-      this.logger.log("Loop not running. Starting.", true);
-      this.running = true;
-      this.updater.refreshLists(function (l) {
-        _this.sendSocketNotification("bringList-LISTUPDATE", l);
-      });
-      this.currentInterval = setInterval(function () {
-        _this.updater.refreshLists(function (l) {
-          _this.sendSocketNotification("bringList-LISTUPDATE", l);
-        });
-      }, this.intervalTime);
+      logBring("Loop not running. Starting.")
+      this.running = true
+      this.updater.refreshLists(l => {
+        this.sendSocketNotification("bringList-LISTUPDATE", l)
+      })
+      this.currentInterval = setInterval(() => {
+        this.updater.refreshLists(l => {
+          this.sendSocketNotification("bringList-LISTUPDATE", l)
+        })
+      }, this.intervalTime)
     }
   },
 
   stop: function () {
-    this.stopLoop();
+    this.stopLoop()
   }
 });
