3a0e8d60b204db2eaa43763be955914120154da9
bugsounet <github@bugsounet.fr>
2022-03-17 01:14:24 +0100

    Dev

--- toDelete/bring-module-config.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.BringModuleConfig = void 0;
-var BringModuleConfig = /** @class */ (function () {
-    function BringModuleConfig() {
-    }
-    return BringModuleConfig;
-}());
-exports.BringModuleConfig = BringModuleConfig;
